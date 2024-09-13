18025a3d50ae129bd321392d0bec10e870cfd6f4
SeniorCub <farinderif@gmail.com>
2024-09-13 22:05:46 +0100

    worked more on the section, converting it to a reusable components

--- notion/src/components/NextSection1.jsx
+++ notion/src/components/NextSection1.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header"
+import SmallBox from "./SmallBox"
 import wiki from "../assets/images/wiki-template.png"
 import { FaBookOpen } from "react-icons/fa"
 
@@ -6,18 +7,17 @@ const NextSection1 = () => {
      return (
      <>
           <div className="container my-16">
-               <Header title={"Start with a template. Build anything."} linkText={"Browse all templates"} style={"w-2/3"} />
-               <div className="grid grid-cols-2 gap-3">
-                    <div className="bg-gray2 p-5 rounded-xl pb-0">
-                         <div>
-                              <div className="mb-5">
-                                   <div><FaBookOpen className="text-red-500 text-4xl" /></div>
-                                   <a href="#" className="flex justify-start items-center text-black gap-1">Company Wiki <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4"><path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg></a>
-                              </div>
-                              <div><img src={wiki} alt="" /></div>
-                         </div>
+               <Header title={"Start with a template. Build anything."} linkText={"Browse all templates"} style={"w-2/3 mb-5"} />
+               <div className="grid grid-cols-2 gap-3 max-h-[10vh]">
+                    <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} img={wiki} />
+                    <div className="grid grid-cols-2 gap-3">
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
                     </div>
-                    <div className="grid grid-cols-2 gap-3"></div>
                </div>
           </div>
      </>
--- notion/src/components/SmallBox.jsx
+++ notion/src/components/SmallBox.jsx
@@ -1,10 +1,18 @@
+import wiki from "../assets/images/wiki-template.png"
+import { FaBookOpen } from "react-icons/fa"
 
-const SmallBox = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const SmallBox = ({icon, name, img}) => {
+     return (
+     <>
+          <div className="bg-gray2 p-7 rounded-xl pb-0">
+               <div className="mb-8 space-y-4">
+                    <div>{icon}</div>
+                    <a href="#" className="flex justify-start items-center text-black gap-1 font-bold text-lg">{name} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 font-bold"><path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg></a>
+               </div>
+               <div><img src={img} alt="" /></div>
+          </div>
+     </>
+     )
 }
 
 export default SmallBox
