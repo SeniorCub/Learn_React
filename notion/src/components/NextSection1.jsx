ec411524392b43e3e4298b399f59232df76cfa53
SeniorCub <farinderif@gmail.com>
2024-09-14 18:28:57 +0100

    Update NextSection1.jsx

--- notion/src/components/NextSection1.jsx
+++ notion/src/components/NextSection1.jsx
@@ -1,22 +1,22 @@
 import Header from "./Header"
 import SmallBox from "./SmallBox"
 import wiki from "../assets/images/wiki-template.png"
-import { FaBookOpen } from "react-icons/fa"
+import { FaBookOpen, FaBicycle, FaGgCircle, FaCalendarDay, FaTicketAlt, FaNewspaper, FaCheckCircle } from "react-icons/fa"
 
 const NextSection1 = () => {
      return (
      <>
           <div className="container my-16">
                <Header title={"Start with a template. Build anything."} linkText={"Browse all templates"} style={"w-2/3 mb-5"} />
-               <div className="grid grid-cols-2 gap-3 max-h-[10vh]">
+               <div className="grid grid-cols-2 gap-5 ">
                     <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} img={wiki} />
-                    <div className="grid grid-cols-2 gap-3">
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
-                         <SmallBox name={"Comapny Wiki"} icon={<FaBookOpen className="text-red-500 text-5xl" />} />
+                    <div className="grid grid-cols-2 gap-5">
+                         <SmallBox name={"Project Roadmap"} icon={<FaBicycle className="text-blue-500 text-5xl" />} />
+                         <SmallBox name={"OKRs"} icon={<FaGgCircle className="text-orange-500 text-5xl" />} />
+                         <SmallBox name={"Meeting Notes"} icon={<FaNewspaper className="text-yellow-500 text-5xl" />} />
+                         <SmallBox name={"Vacation Planner"} icon={<FaTicketAlt className="text-red-500 text-5xl" />} />
+                         <SmallBox name={"Editorial Calendar"} icon={<FaCalendarDay className="text-blue-500 text-5xl" />} />
+                         <SmallBox name={"Habit Tracker"} icon={<FaCheckCircle className="text-purple-500 text-5xl" />} />
                     </div>
                </div>
           </div>
