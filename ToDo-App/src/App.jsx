ddea91d7e75f86151f351b063c254d76106279a1
seniorcub <farinderif@gmail.com>
2024-09-27 23:29:04 +0100

    added router

--- ToDo-App/src/App.jsx
+++ ToDo-App/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import ListView from "./pages/ListView"
+import Error from './pages/Error'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
      return (
      <>
+      <Routes>
+
           <ListView />
+          {/* Error 404 Page */}
+          <Route path='*' element={<Error />} />
+      </Routes>
      </>
      )
 }
