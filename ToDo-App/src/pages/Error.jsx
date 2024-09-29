be7ad9872718d348a20a799e664eb7a4d27b8964
seniorcub <farinderif@gmail.com>
2024-09-29 14:26:04 +0100

    trying to work on routing

--- ToDo-App/src/pages/Error.jsx
+++ ToDo-App/src/pages/Error.jsx
@@ -1,4 +1,4 @@
-import Button from "../components/Button"
+// import Button from "../components/Button"
 import img from "../assets/images/amico.0f21f32000ef9ea2915a.png"
 
 const Error = () => {
@@ -9,7 +9,7 @@ const Error = () => {
                          <h1 className="text-red-500 text-5xl font-bold">Oh No!</h1>
                          <p>Something is gone missing.</p>
                          <p>Nothing is found here! You can check your Internet connection or check back later.</p>
-                         <Button name={'Go Back'} style={'bg-black text-white'} link={'/'}/>
+                         {/* <Button name={'Go Back'} style={'bg-black text-white'} link={'/'}/> */}
                     </div>
                     <div>
                          <img src={img} alt="" />
